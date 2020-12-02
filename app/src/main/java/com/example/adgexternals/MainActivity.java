3dad9533046ed1361058ff19daa4833430da8186
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-12-02 15:20:00 +0530

    added navbar and fragments

--- app/src/main/java/com/example/adgexternals/MainActivity.java
+++ app/src/main/java/com/example/adgexternals/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.adgexternals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new homefragment()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedfragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        selectedfragment = new homefragment();
+                        break;
+                    case R.id.navigation_recruitment:
+                        selectedfragment = new recruitmentfragment();
+                        break;
+                    case R.id.navigation_faq:
+                        selectedfragment = new faq_fragment();
+                        break;
+                    case R.id.navigation_settings:
+                        selectedfragment = new settings_fragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedfragment).commit();
+                return  true;
+            }
+        });
     }
 }
