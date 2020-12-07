62ae0945915d74f87212d3cd7e9eeb396f0a8139
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-12-07 17:40:50 +0530

    added objects for question get and answer post

--- app/src/main/java/com/example/adgexternals/userClient.java
+++ app/src/main/java/com/example/adgexternals/userClient.java
@@ -1,5 +1,7 @@
 package com.example.adgexternals;
 
+import java.util.List;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -19,19 +21,30 @@ public interface userClient {
     Call<root> getUser(@Header("auth-token") String authtoken);
 
     @GET("management/get-random-questions")
-    Call<ResponseBody> getQuestionManagement(@Header("auth-token") String authtoken);
+    Call<List<questionObject>> getQuestionManagement(@Header("auth-token") String authtoken);
 
     @GET("design/get-random-questions")
-    Call<ResponseBody> getQuestionDesign(@Header("auth-token") String token);
+    Call<List<questionObject>> getQuestionDesign(@Header("auth-token") String token);
 
     @GET("technical/get-random-questions")
-    Call<ResponseBody> getQuestionTechnical(@Header("auth-token") String token);
+    Call<questionObjectTechnical> getQuestionTechnical(@Header("auth-token") String token);
 
-    //@POST("management/submit")
+    @POST("management/submit")
+    Call<postQuestion> postQuestionManagement(
+            @Header("auth-token") String authtoken,
+            @Body postQuestion ques
+    );
 
-    //@POST("design/submit")
+    @POST("design/submit")
+    Call<postQuestion> postQuestionDesign(
+            @Header("auth-token") String authtoken,
+            @Body postQuestion ques
+    );
 
-    //@POST("technical/submit")
-
+    @POST("technical/submit")
+    Call<postQuestion> postQuestionTechnical(
+            @Header("auth-token") String authtoken,
+            @Body postQuestion ques
+    );
 
 }
