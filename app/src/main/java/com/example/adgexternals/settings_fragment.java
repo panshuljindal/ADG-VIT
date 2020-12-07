9677ddd3c1a0e77ef2b02e1bb7d431a9ac00aa3c
arupam <sahaarupam37@gmail.com>
2020-12-07 23:41:41 +0530

    add shared Transition for profile pic

--- app/src/main/java/com/example/adgexternals/settings_fragment.java
+++ app/src/main/java/com/example/adgexternals/settings_fragment.java
@@ -1,20 +1,26 @@
 package com.example.adgexternals;
 
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.transition.Fade;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class settings_fragment extends Fragment {
-    Button editProfile;
+    Button editProfile; ImageView profilepic1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,14 +34,20 @@ public class settings_fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_settings_fragment, container, false);
 
         editProfile = (Button) view.findViewById(R.id.editProfileBtn);
+        profilepic1 = (ImageView) view.findViewById(R.id.profile_image1);
 
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Toast.makeText(view.getContext(), "edit profile", Toast.LENGTH_SHORT).show();
                 editProfileFragment nextFrag= new editProfileFragment();
+
                 FragmentManager fragmentManager = ((FragmentActivity) view.getContext()).getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                nextFrag.setSharedElementEnterTransition(new DetailsTransition());
+                nextFrag.setSharedElementReturnTransition(new DetailsTransition());
+                fragmentTransaction.addSharedElement(profilepic1,"sharedPIC");
                 fragmentTransaction.replace(R.id.frameLayout,nextFrag);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
