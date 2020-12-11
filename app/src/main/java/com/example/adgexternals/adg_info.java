baa6b85d3ad5519a69d56f73ae980c4143577328
arupam <sahaarupam37@gmail.com>
2020-12-11 17:44:09 +0530

    add social click listeners adginfo

--- app/src/main/java/com/example/adgexternals/adg_info.java
+++ app/src/main/java/com/example/adgexternals/adg_info.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class adg_info extends AppCompatActivity {
     Button cancel;
+    ImageView adgFb , adgTwitter , adgInsta , adgLinkedin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adg_info);
 
+        adgFb = findViewById(R.id.adgFacebook);
+        adgTwitter = findViewById(R.id.adgTwitter);
+        adgInsta = findViewById(R.id.adgInstagram);
+        adgLinkedin = findViewById(R.id.adgLinkedin);
+
         cancel = findViewById(R.id.buttonInfoCancel);
+
+        onClickListener();
+
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +33,37 @@ public class adg_info extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void onClickListener() {
+
+        adgLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        adgFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        adgTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        adgInsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
