ea567a8498dcba22d57a66e3c0f5a6211c99c590
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-12-12 14:55:55 +0530

    added faq type fragments

--- app/src/main/java/com/example/adgexternals/about_us_fragment.java
+++ app/src/main/java/com/example/adgexternals/about_us_fragment.java
@@ -193,7 +193,7 @@ public class about_us_fragment extends Fragment {
     }
     public void addData(){
         list.add(new aboutUs("Panshul Jindal","https://github.com/panshuljindal","https://www.linkedin.com/in/panshul-jindal-392746199","panshuljindal@gmail.com"));
-        list.add(new aboutUs("Ritik Suryawanshi","https://github.com/rajritik2607","https://www.linkedin.com/in/ritik-suryawanshi-7075441a6","mailto:ritik.suryawanshi@gmail.com"));
+        list.add(new aboutUs("Ritik Suryawanshi","https://github.com/rajritik2607","https://www.linkedin.com/in/ritik-suryawanshi-7075441a6","ritik.suryawanshi@gmail.com"));
         list.add(new aboutUs("Arupam Kumar Saha","https://github.com/arupam","https://www.linkedin.com/in/arupam-kumar-saha-310653191/","sahaarupam37@gmail.com"));
         list.add(new aboutUs("Gokul Nair","https://github.com/gokulnair2001","https://www.linkedin.com/in/gokul-r-nair/","gokulnair.2001@gmail.com"));
         list.add(new aboutUs("Krishna Khanikar","https://github.com/krishnakhanikar","https://www.linkedin.com/in/krishna-khanikar-a0144a19a/","krishna.khanikar2019@vitstudent.ac.in"));
