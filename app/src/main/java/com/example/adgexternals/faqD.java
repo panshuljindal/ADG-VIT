1b09e4928a2b4958a97672982a03d869cedebb66
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-12-12 15:44:21 +0530

    added faq types recyclerview

--- app/src/main/java/com/example/adgexternals/faqD.java
+++ app/src/main/java/com/example/adgexternals/faqD.java
@@ -3,62 +3,53 @@ package com.example.adgexternals;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link faqD#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class faqD extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public faqD() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment faqD.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static faqD newInstance(String param1, String param2) {
-        faqD fragment = new faqD();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private RecyclerView recyclerView;
+    private List<faqitem> list;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faq_d, container, false);
+        View view = inflater.inflate(R.layout.fragment_faq_d, container, false);
+
+        Button back = view.findViewById(R.id.back_button_faqD);
+        recyclerView = view.findViewById(R.id.recyclerView_faqD);
+
+        list=new ArrayList<>();
+        addData();
+
+        faqadapter adapter = new faqadapter(list,getContext());
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().popBackStackImmediate();
+            }
+        });
+        return view;
+    }
+    void addData(){
+        list.add(new faqitem("Hello","Panshul"));
     }
 }
--- app/src/main/java/com/example/adgexternals/faqitem.java
+++ app/src/main/java/com/example/adgexternals/faqitem.java
@@ -1,4 +1,27 @@
 package com.example.adgexternals;
 
 public class faqitem {
+    String question;
+    String answer;
+
+    public faqitem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
