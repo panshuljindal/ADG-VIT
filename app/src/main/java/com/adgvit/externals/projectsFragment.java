5b5150888ca4c6129c25cfbfe0b63d0b92e73e44
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2020-12-13 22:02:41 +0530

    added back in projects

--- app/src/main/java/com/adgvit/externals/projectsFragment.java
+++ app/src/main/java/com/adgvit/externals/projectsFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class projectsFragment extends Fragment {
 
@@ -19,6 +20,16 @@ public class projectsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_projects, container, false);
+        View view =  inflater.inflate(R.layout.fragment_projects, container, false);
+
+        Button back = view.findViewById(R.id.buttonprojectback);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().popBackStackImmediate();
+            }
+        });
+
+        return view;
     }
 }
